f7e81d916110f3c46d210bdba7a0de5556ccc138
MMallory17 <mdmallory87@gmail.com>
2024-05-01 16:55:13 -0400

    first commit

---
+++ Exponents Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+
+Console.WriteLine("Learn your Squares and Cubes!");
+
+Console.Write("Enter an integer:"); int number = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Number \t\t Squared \t Cubed");
+    Console.WriteLine("========  \t===========  \t=========");
+
+MathF.Pow(i, 2);
+MathF.Pow(number, 3);
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine($"{i,10} \t{number} 10}}");
+}
+
+
+
